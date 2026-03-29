f5a32a754c650c6a02758430cbc509999ca853fa
Marylene-ALC <m.sugiraine@alustudent.com>
2026-03-29 09:43:29 -0700

    Added the file

--- io_redirections_and_filters/work.sh
+++ io_redirections_and_filters/work.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-read -p "Enter the file name " file.
-read -p "Enter the command " cmd.
+
+read -p "Enter the file name" file.
+read -p "Enter the command" cmd.
 
 echo -e "#!/bin/bash\n${cmd}" > $file.
 chmod u+x ${file}
