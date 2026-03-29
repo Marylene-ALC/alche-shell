d3df4a8c9c1cb24cf0e9cfde7637df34d7b1c63f
Marylene-ALC <m.sugiraine@alustudent.com>
2026-03-29 09:55:10 -0700

    Added the file 8-cwd_state

--- io_redirections_and_filters/work.sh
+++ io_redirections_and_filters/work.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-read -p "Enter the file name" file.
-read -p "Enter the command" cmd.
+read -p "Enter the file name: " file
+read -p "Enter the command: " cmd
 
-echo -e "#!/bin/bash\n${cmd}" > $file.
+echo -e "#!/bin/bash\n${cmd}" > $file
 chmod u+x ${file}
 
 git add .
